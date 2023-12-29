4a1636e5a47aea0a4e9249fa36fb09f4ec0dfa6d
gorazd007 <gorazdtezak@gmail.com>
2023-12-29 09:38:18 +0100

    start using context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,8 @@
+import { useContext } from 'react'
+import BooksContext from '../context/books'
 import BookShow from './BookShow'
 
-export default function BookList({books, deleteBookById, editBookById }) {
+export default function BookList({ books, deleteBookById, editBookById }) {
 
     const renderBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} deleteBookById={deleteBookById} editBookById={editBookById} />
---
+++ src/context/books.js
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+const BooksContext = createContext()
+
+const Provider = ({ children }) => {
+    
+    return <BooksContext.Provider value={{}}>
+        {children}
+    </BooksContext.Provider>
+}
+
+export { Provider }
+export default BooksContext
